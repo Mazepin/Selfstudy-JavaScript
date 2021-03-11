8c18e3e915e537374aa4acf840f07a1e0235ce5a
Igor <75262127+Mazepin@users.noreply.github.com>
2021-03-11 20:26:40 +0300

    Update all.js
    add function in js file

--- WebsiteWithCSS/all.js
+++ WebsiteWithCSS/all.js
@@ -1,7 +1,14 @@
+function validate(evt){
+    return confirm("are you sure?");
+ }
+
+
 $(".btn").click(function() {
     var myBtn = this;
     var main = $(".main");
     if (myBtn.id == "1") {
         main.setStyle("style1");
     }
+
+    
 })

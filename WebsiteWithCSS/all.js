19b5cb08722a3163dcc893297d20ce183161da6f
Igor <75262127+Mazepin@users.noreply.github.com>
2021-03-12 23:13:55 +0300

    Update all.js

--- WebsiteWithCSS/all.js
+++ WebsiteWithCSS/all.js
@@ -1,6 +1,14 @@
 function validate(evt){
     return confirm("are you sure?");
- }
+}
+
+// function validSex(){
+//     var state = $("input[name='sex']:checked").val();
+//         if(!state) return $("#message").html("<font style='color:red' font-size='5pt'>Please enter values in all fields!</font><br />");
+//         $("#message").hide();
+           
+    
+// }
 
 
 $(".btn").click(function() {
